397ae00bf804f97dd0b8d9fc3b9ea881619b5f67
EmilyYVChan <emily97m2@gmail.com>
2018-09-12 03:39:45 +1200

    Some initial React Router for Login page and Edit page

---
+++ src/components/EditPage.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class EditPage extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>Edit Page</h1>
+      </div>
+    );
+  }
+}
+
+export default EditPage;
