2ddcd212aca5bb0016a4b0ed64eb0625d8c30fb4
EmilyYVChan <emily97m2@gmail.com>
2018-09-12 03:40:58 +1200

    Restart from blank template

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
 import React, { Component } from "react";
-import { BrowserRouter, Route } from "react-router-dom";
-import LoginPage from "./components/LoginPage";
-import EditPage from "./components/EditPage";
 
 class App extends Component {
   render() {
-    return (
-      <BrowserRouter>
-        <div>
-          <Route exact path="/" component={LoginPage} />
-          <Route path="/edit" component={EditPage} />
-        </div>
-      </BrowserRouter>
-    );
+    return <div />;
   }
 }
 
